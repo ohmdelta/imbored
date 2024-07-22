a2ee7d5c29f121af6f97b589057ab053a5712640
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-07-22 02:24:31 +0100

    add coordinate class

--- lin_alg/coordinate.hpp
+++ lin_alg/coordinate.hpp
@@ -17,7 +17,6 @@ namespace lin_alg
             double z_,
             double a_ = 1.0) : x(x_), y(y_), z(z_), a(a_)
         {
-
         }
 
         double x;
@@ -25,7 +24,7 @@ namespace lin_alg
         double z;
         double a;
 
-        double dot(const Vector & vector);
+        double dot(const Vector &vector);
         double sum();
 
         double &operator[](int __n);
@@ -34,10 +33,10 @@ namespace lin_alg
         double &operator()(int __n) { return operator[](__n); };
         const double &operator()(int __n) const { return operator[](__n); };
 
-        void normalise();
+        virtual void normalise();
 
-        double norm();
-        double norm_sq();
+        virtual double norm();
+        virtual double norm_sq();
 
         Vector operator*(double p);
         Vector operator*(double p) const;
@@ -72,6 +71,45 @@ namespace lin_alg
 
         friend std::ostream &operator<<(std::ostream &Str, Vector &mc);
     };
+
+    class Coordinate : public Vector
+    {
+    public:
+        Coordinate(
+            double x_,
+            double y_,
+            double z_,
+            double a_ = 1.0) : Vector(x_, y_, z_, a_)
+        {
+        }
+
+        void normalise()
+        {
+            if (a == 1)
+                return;
+
+            if (a == 0)
+            {
+                throw std::runtime_error("Math error: Attempted to divide by Zero\n");
+            }
+
+            x /= a;
+            y /= a;
+            z /= a;
+        }
+
+        double norm_sq()
+        {
+            normalise();
+            return x * x + y * y + z * z;
+        }
+
+        double norm()
+        {
+            return sqrt(norm_sq());
+        }
+    };
+
 }
 
 #endif
