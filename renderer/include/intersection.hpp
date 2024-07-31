0b80d18ae7f8680d5de9f7a7fce73327a837a74b
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-07-31 02:36:05 +0100

    update polynomial tests

--- renderer/include/intersection.hpp
+++ renderer/include/intersection.hpp
@@ -4,37 +4,30 @@
 #include <cmath>
 #include <memory>
 
-#include "ray.hpp"
 #include "lin_alg.hpp"
+#include "ray.hpp"
 
-namespace renderer
-{
+namespace renderer {
 
-    struct Intersection
-    {
-        Intersection(
-            bool valid_,
-            const lin_alg::Coordinate &coordinate_ = lin_alg::Origin(),
-            double ray_length_ = 0.0,
-            size_t id_ = -1) : valid(valid_),
-                               coordinate(coordinate_),
-                               ray_length(ray_length_),
-                               id(id_)
-        {
-        }
+struct Intersection {
+  Intersection(bool valid_,
+               const lin_alg::Coordinate &coordinate_ = lin_alg::Origin(),
+               double ray_length_ = 0.0, size_t id_ = -1)
+      : valid(valid_), coordinate(coordinate_), ray_length(ray_length_),
+        id(id_) {}
 
-        bool valid;
-        lin_alg::Coordinate coordinate;
-        double ray_length;
-        size_t id = -1;
-        lin_alg::Coordinate normal = lin_alg::Origin();
+  bool valid;
+  lin_alg::Coordinate coordinate;
+  double ray_length;
+  size_t id = -1;
+  lin_alg::Coordinate normal = lin_alg::Origin();
 
-        double max_length = 1000;
+  double max_length = 1000;
 
-        Ray reflected_ray(Ray ray);
+  Ray reflected_ray(Ray ray);
 
-        friend bool operator<(const Intersection &l, const Intersection &r);
-    };
+  friend bool operator<(const Intersection &l, const Intersection &r);
 };
+}; // namespace renderer
 
 #endif
